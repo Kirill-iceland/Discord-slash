d498b5e88fdfeb616b836edf5d7ff2f8e214e8f8
Kirill-iceland <58794914+Kirill-iceland@users.noreply.github.com>
2021-04-19 15:43:14 +0000

    Added command classes + toJSON()

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,6 @@
 type callback = (res: any) => any
 
+
 declare enum CommandOptionTypes {
     SUB_COMMAND,
     SUB_COMMAND_GROUP,
@@ -14,7 +15,13 @@ declare enum CommandOptionTypes {
 declare module '@the_zolotuskiy/discord-slash' {
     import Discord from 'discord.js'
 
-    type CommandOptions = SubCommand | SubCommandGroup | CommandArguments
+    type CommandOptions = SubCommand | SubCommandGroup | CommandArgument
+    type CommandOptionsJSON = SubCommandJSON | SubCommandGroupJSON | CommandArgumentJSON
+
+    type CommandJSON = {name: string, description?: string, options: CommandOptionsJSON[]}
+    type SubCommandGroupJSON = {name: string, type: 2, description?: string, options: (CommandArgument | SubCommand)[]}
+    type SubCommandJSON = {name: string, type: 1, description?: string, options: CommandArgument[]}
+    type CommandArgumentJSON = {name: string, type: 3 | 4 | 5 | 6 | 7 | 8, required: boolean, description?: string, choices: {name: string, value: string}[]}
 
     export class Command{
         constructor(name: string, description?: string, options?: CommandOptions[])
@@ -26,50 +33,61 @@ declare module '@the_zolotuskiy/discord-slash' {
         public setName(name: string): this
         public setDescription(description: string): this
         
-        public setOptions(options: CommandArguments[]): this 
-        public addOptions(options: CommandArguments[]): this 
-        public addOption(option: CommandArguments): this 
+        public setOptions(options: CommandArgument[]): this 
+        public addOptions(options: CommandArgument[]): this 
+        public addOption(option: CommandArgument): this 
 
-        public toJSON(): {name: string, description?: string, options: {}[]}
+        public toJSON(): CommandJSON
     }
 
 
 
     export class CommandOption{
+        constructor(name: string, description?: string)
         public name: string
         public description: string
         public type: number
 
         public setName(name: string): this
         public setDescription(description: string): this
-        public setType(type: number): this
+        public setType(type: number): CommandOptions
     }
 
     export class SubCommand extends CommandOption{
+        constructor(name: string, description?: string, options?: CommandArgument[])
         public type: 1
-        public options: CommandArguments[]
+        public options: CommandArgument[]
 
-        public setOptions(options: CommandArguments[]): this 
-        public addOptions(options: CommandArguments[]): this 
-        public addOption(option: CommandArguments): this 
+        public setOptions(options: CommandArgument[]): this 
+        public addOptions(options: CommandArgument[]): this 
+        public addOption(option: CommandArgument): this 
+
+        public toJSON(): SubCommandJSON
     }
     export class SubCommandGroup extends CommandOption{
+        constructor(name: string, description?: string, options?: (CommandArgument | SubCommand)[])
         public type: 2
-        public options: CommandArguments[] | SubCommand[]
+        public options: (CommandArgument | SubCommand)[]
 
-        public setOptions(options: CommandArguments[] | SubCommand[]): this 
-        public addOptions(options: CommandArguments[] | SubCommand[]): this 
-        public addOption(option: CommandArguments | SubCommand): this 
+        public setOptions(options: (CommandArgument | SubCommand)[]): this 
+        public addOptions(options: (CommandArgument | SubCommand)[]): this 
+        public addOption(option: CommandArgument | SubCommand): this 
+        
+        public toJSON(): SubCommandGroupJSON
     }
-    export class CommandArguments extends CommandOption{
+    export class CommandArgument extends CommandOption{
+        constructor(name: string, description?: string, options?: CommandArgument[])
         public type: 3 | 4 | 5 | 6 | 7 | 8
         public required: boolean
         public choices: {name: string, value: string}[]
 
+        public setType(type: number): this
         public setRequired(type: boolean): this 
         public setChoices(choices: {name: string, value: string}[]): this 
         public addChoices(choices: {name: string, value: string}[]): this 
         public addChoice(name: string, value: string): this 
+        
+        public toJSON(): CommandArgumentJSON
     }
 
 
--- index.js
+++ index.js
@@ -10,10 +10,10 @@ class Command{
         if(options.length > 25){
             console.error('Command can only have 25 options!')
         }
-        if(options.length > 1){
-            this.options = options;
-        }else{
+        if(options.length == 1){
             this.options = [options];
+        }else{
+            this.options = options;
         }
     }
     
@@ -43,34 +43,254 @@ class Command{
         }
         this.options.push(option)
     }
-}
-exports.BaceCommand
 
-exports = {
-    post: async (client, command, callback = calback) => {
-        return callback(await client.api.applications(client.user.id).commands.post({data: command.toJSON()}))
-    },
-    edit: async (client, command, callback = calback) => {
-        return callback(await client.api.applications(client.user.id).commands.patch({data: command.toJSON()}))
-    },
-    get: async (client, callback = calback) => {
-        return callback(await client.api.applications(client.user.id).commands.get())
-    },
-    delete_: async (client, id, callback = calback) => {
-        return callback(client.api.applications(client.user.id).commands(id).delete())
-    },
-
-    //Guild
-    GuildPost: async (client, guild, command, callback = calback) => {
-        return callback(await client.api.applications(client.user.id).guilds(guild.id).commands.post({data: command.toJSON()}))
-    },
-    GuildEdit: async (client, guild, command, callback = calback) => {
-        return callback(await client.api.applications(client.user.id).guilds(guild.id).commands.patch({data: command.toJSON()}))
-    },
-    GuildGet: async (client, guild, callback = calback) => {
-        return callback(await client.api.applications(client.user.id).guilds(guild.id).commands.get())
-    },
-    GuildDelete: async (client, guild, id, callback = calback) => {
-        return callback(client.api.applications(client.user.id).guilds(guild.id).commands(id).delete())
+    toJSON(){
+        let options = [];
+        this.options.forEach(option => {
+            options.push(option.toJSON())
+        })
+        return {name: this.name, description: this.description, options}
     }
+}
+exports.Command = Command
+
+class CommandOption{
+    constructor(name, description = ""){
+        this.name = name;
+        this.description = description;
+
+    }
+    
+    setName(name){
+        this.name = name;
+    }
+    setDescription(description){
+        this.description = description;
+    }
+    setType(type){
+        if(typeof type != 'number' || type > 8 || type < 1){
+            console.error('Wrong type');
+            return;
+        }
+        this.type = type
+        switch(type){
+            case 1:
+                return new SubCommand(this.name, this.type, this.description);
+            case 2:
+                return new SubCommandGroup(this.name, this.type, this.description);
+            default:
+                return new CommandArguments(this.name, this.type, this.description);
+        }
+    }
+}
+exports.CommandOption = CommandOption
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class SubCommand extends CommandOption{
+    constructor(name, description = "", options = []){
+        this.name = name;
+        this.description = description;
+        this.type = 1;
+
+        if(options.length > 25){
+            console.error('Sub Command can only have 25 options!')
+        }
+        if(options.length == 1){
+            this.options = [options];
+        }else{
+            this.options = options;
+        }
+    }
+
+    setOptions(options){
+        if(options.length > 25){
+            console.error('Sub Command can only have 25 options!')
+        }
+        this.options = options;
+    }
+    addOptions(options){
+        if(this.options.length + options.length > 25){
+            console.error('Sub Command can only have 25 options!')
+        }
+        this.options = this.options.concat(options)
+    }
+    addOption(option){
+        if(this.options.length == 25){
+            console.error('Sub Command can only have 25 options!')
+        }
+        this.options.push(option);
+    }
+
+    toJSON(){
+        let options = [];
+        this.options.forEach(option => {
+            options.push(option.toJSON());
+        })
+        return {name: this.name, type: this.type, description: this.description, options}
+    }
+}
+
+//---------------------------------------------------------------------------------------------------------------
+class SubCommandGroup extends CommandOption{
+    constructor(name, description = "", options = []){
+        this.name = name;
+        this.description = description;
+        this.type = 2;
+
+        if(options.length > 25){
+            console.error('Sub Command can only have 25 options!')
+        }
+        if(options.length == 1){
+            this.options = [options];
+        }else{
+            this.options = options;
+        }
+    }
+
+    setOptions(options){
+        if(options.length > 25){
+            console.error('Sub Command can only have 25 options!')
+        }
+        this.options = options;
+    }
+    addOptions(options){
+        if(this.options.length + options.length > 25){
+            console.error('Sub Command can only have 25 options!')
+        }
+        this.options = this.options.concat(options);
+    }
+    addOption(option){
+        if(this.options.length == 25){
+            console.error('Sub Command can only have 25 options!')
+        }
+        this.options.push(option);
+    }
+
+    toJSON(){
+        let options = [];
+        this.options.forEach(option => {
+            options.push(option.toJSON())
+        })
+        return {name: this.name, type: this.type, description: this.description, options};
+    }
+}
+exports.SubCommandGroup = SubCommandGroup;
+
+//---------------------------------------------------------------------------------------------------------------
+class CommandArgument extends CommandOption{
+    constructor(name,  type, required, description = "", choices = []){
+        this.name = name;
+        this.description = description;
+        this.type = type;
+        this.required = required;
+
+        if(choices.length > 25){
+            console.error('Sub Command can only have 25 choices!')
+        }
+        if(choices.length == 1){
+            this.choices = [choices];
+        }else{
+            this.choices = choices;
+        }
+    }
+
+    setType(type){
+        if(typeof type != 'number' || type > 8 || type < 3){
+            console.error('Wrong type');
+            return;
+        }
+        this.type = type;
+    }
+    setRequired(required){
+        if(typeof required != 'boolean'){
+            console.error('Wrong type');
+            return;
+        }
+        this.required = required;
+    }
+    setChoices(choices){
+        if(choices.length > 25){
+            console.error('Sub Command can only have 25 choices!')
+        }
+        this.choices = choices;
+    }
+    addChoices(choices){
+        if(this.choices.length + choices.length > 25){
+            console.error('Sub Command can only have 25 choices!')
+        }
+        this.choices = this.choices.concat(choices);
+    }
+    addChoices(choices){
+        if(this.choices.length == 25){
+            console.error('Sub Command can only have 25 choices!')
+        }
+        this.choices.push(choices);
+    }
+
+    toJSON(){
+        let choices = [];
+        this.choices.forEach(choice => {
+            choices.push(choice);
+        })
+        return {name: this.name, type: this.type, required: this.required, description: this.description, choices};
+    }
+}
+exports.CommandArgument = CommandArgument;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+exports.post = async (client, command, callback = calback) => {
+    return callback(await client.api.applications(client.user.id).commands.post({data: command.toJSON()}))
+}
+exports.edit = async (client, command, callback = calback) => {
+    return callback(await client.api.applications(client.user.id).commands.patch({data: command.toJSON()}))
+}
+exports.get = async (client, callback = calback) => {
+    return callback(await client.api.applications(client.user.id).commands.get())
+}
+exports.delete_ = async (client, id, callback = calback) => {
+    return callback(client.api.applications(client.user.id).commands(id).delete())
+}
+
+//Guild
+exports.GuildPost = async (client, guild, command, callback = calback) => {
+    return callback(await client.api.applications(client.user.id).guilds(guild.id).commands.post({data: command.toJSON()}))
+}
+exports.GuildEdit = async (client, guild, command, callback = calback) => {
+    return callback(await client.api.applications(client.user.id).guilds(guild.id).commands.patch({data: command.toJSON()}))
+}
+exports.GuildGet = async (client, guild, callback = calback) => {
+    return callback(await client.api.applications(client.user.id).guilds(guild.id).commands.get())
+}
+exports.GuildDelete =  async (client, guild, id, callback = calback) => {
+    return callback(client.api.applications(client.user.id).guilds(guild.id).commands(id).delete())
 }
